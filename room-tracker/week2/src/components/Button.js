6a3e6620bf4d3e64b655c2f2180fcb9cf8e4b60e
Ryan Gaudion <43444172+RyanGaudion@users.noreply.github.com>
2022-10-05 07:53:08 +0100

    Added week 2

---
+++ room-tracker/week2/src/components/Button.js
@@ -0,0 +1,12 @@
+export default function Button({ onClick, children }) {
+  return (
+   <>
+    <a
+      onClick={onClick}
+      className="blue-button"
+    >
+	   {children}
+    </a>
+    </>
+  );
+}
---
+++ room-tracker/week2/src/mocks/data.js
@@ -0,0 +1,80 @@
+export const buildings = [
+  {
+    name: "John Everett Millais",
+    code: "JM",
+  },
+  {
+    name: "Michael Andrews",
+    code: "A",
+  },
+  {
+    name: "Reginald Mitchell",
+    code: "RM",
+  },
+  {
+    name: "Sir Christopher Cockerell",
+    code: "CC",
+  },
+];
+
+export const roomTypes = [
+  {
+    name: "PC Lab",
+    code: "pc",
+  },
+  {
+    name: "Mac Lab",
+    code: "mac",
+  },
+  {
+    name: "Lecture Theatre",
+    code: "lt",
+  },
+];
+
+export const rooms = [
+  {
+    id: 1,
+    number: "HC208",
+    building: "HC",
+    capacity: 15,
+    type: { code: "pc", name: "PC Lab" },
+  },
+  {
+
+    id: 2,
+    number: "HC208",
+    building: "HC",
+    capacity: 10,
+    type: { code: "pc", name: "PC Lab" },
+  },
+  {
+    id: 3,
+    number: "HC208",
+    building: "HC",
+    capacity: 5,
+    type: { code: "pc", name: "PC Lab" },
+  },
+  {
+
+    id: 4,
+    number: "HC208",
+    building: "HC",
+    capacity: 35,
+    type: { code: "pc", name: "PC Lab" },
+  },
+  {
+    id: 5,
+    number: "HC208",
+    building: "HC",
+    capacity: 18,
+    type: { code: "pc", name: "PC Lab" },
+  },
+  {
+    id: 6,
+    number: "HC208",
+    building: "HC",
+    capacity: 40,
+    type: { code: "pc", name: "PC Lab" },
+  },
+];
